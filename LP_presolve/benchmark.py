dee70785ae4cdfbc0e6e408dfec20765ae23da94
Touqir <shuhash6@gmail.com>
2018-12-30 10:55:35 -0700

    adjusted the code for experiments

--- LP_presolve/benchmark.py
+++ LP_presolve/benchmark.py
@@ -1,6 +1,6 @@
 from scipy.io import loadmat
 import numpy as np
-from _matrix_compress import compute_independent_columns 
+from _matrix_compress import compute_independent_columns, _extract_independent_columns, _build_matrix_rank_k_FAST
 import os.path 
 from scipy.optimize import _remove_redundancy
 import time
@@ -10,6 +10,7 @@ import pickle
 import copy
 from pathlib import Path
 import sys
+import scipy.linalg
 
 
 def load_matrix(fileName):
@@ -17,12 +18,14 @@ def load_matrix(fileName):
     rank = M_dict['numrank'][0,0]
     if type(M_dict['A']) == np.ndarray:
         A = M_dict['A']
-        sparse_A = scipy.sparse.csr_matrix(A)
-        return A, sparse_A,  rank
+        # sparse_A = scipy.sparse.csr_matrix(A)
+        # return A, sparse_A,  rank
+        return A, rank
     else:
         sparse_A = M_dict['A']
         A = sparse_A.toarray() 
-        return A, sparse_A, rank
+        # return A, sparse_A, rank
+        return A, rank
 
 
 # def load_matrix_2(fileName, condMultiplier):
@@ -39,14 +42,55 @@ def load_matrix(fileName):
 #     return A, np.linalg.matrix_rank(A)
 
 
-# def remove_redundancy_ID(A, rank):
-#     return _extract_independent_columns(A, rank)
+def remove_redundancy_ID(A, rank):
+    cols = _extract_independent_columns(A, rank=rank)
+    A_new = A[:,cols]
+    rank_new = matrix_rank(A_new)
+    print("remove_redundancy_ID: ", rank_new == rank, rank_new, rank)
+    return rank_new
+
 
 def remove_redundancy_hybrid_ID(A, rank):
-    return compute_independent_columns(A, k=rank)
+    A_new, cols = compute_independent_columns(A, k=rank)
+    rank_new = matrix_rank(A_new)
+    print("remove_redundancy_hybrid_ID: ", rank_new == rank, rank_new, rank)
+    return rank_new
 
-def remove_redundancy_SVD(A):
-    return _remove_redundancy._remove_redundancy(A, np.ones((A.shape[0],1)))
+def remove_redundancy_dense(A, rank):
+    A_new, status = _remove_redundancy_SVD(A.T, rank)
+    A_new = A_new.T
+    rank_new = matrix_rank(A_new)
+    print("remove_redundancy_hybrid_ID: status:", status)
+    print("remove_redundancy_hybrid_ID: Ranks:", rank_new == rank, rank_new, rank)
+    print("remove_redundancy_hybrid_ID: Cols:", rank_new == A_new.shape[1], A_new.shape[1])
+    return rank_new
+
+
+def _remove_redundancy_SVD(A, true_rank):
+
+    U, s, Vh = np.linalg.svd(A)
+    eps = np.finfo(float).eps
+    tol = s.max() * max(A.shape) * eps
+
+    m, n = A.shape
+    s_min = s[-1] if m <= n else 0
+    status = 1
+
+    while abs(s_min) < tol:
+        v = U[:, -1]  
+        i_remove = np.argmax(v)
+        A = np.delete(A, i_remove, axis=0)
+
+        if min(A.shape) < true_rank or np.count_nonzero(s > tol) < true_rank:
+            status = 0
+            break
+
+        U, s, Vh = np.linalg.svd(A)
+        print("A.shape: ", A.shape)
+        m, n = A.shape
+        s_min = s[-1] if m <= n else 0
+
+    return A, status
 
 def remove_redundancy_sparse(sparse_A):
     return _remove_redundancy._remove_redundancy_sparse(sparse_A, np.ones((sparse_A.shape[0],1)))
@@ -79,7 +123,7 @@ def validate_fileNames(fileList, extension=False):
     return all_clear, incorrect_paths
 
 
-def measure_performance():
+def measure_performance(data_saver):
     files, filenames = import_fileNames(fileName, base_location)
     all_clear, incorrect_paths = validate_fileNames(files, extension='.mat')
     
@@ -97,30 +141,51 @@ def measure_performance():
         print("Benchmarking on", file)
 
         try:
-            A, sparse_A, rank = load_matrix(file)
+            # A, sparse_A, rank = load_matrix(file)
+            A, rank = load_matrix(file)
+            # rank = 90
+            # A = _build_matrix_rank_k_FAST(100,100,rank)
+        
+            estimated_rank = matrix_rank(A)
+            performance_dict[filename].append(["Numpy rank and precomputed rank", estimated_rank, rank])
 
-            t1 = time.time()
-            remove_redundancy_hybrid_ID(A, rank)
-            t2 = time.time() - t1
-            print("remove_redundancy_hybrid_ID took: ", t2) 
-            performance_dict[filename].append(["remove_redundancy_hybrid_ID", t2])
+            t1 = time.perf_counter()
+            t2 = time.process_time()
+            hybrid_ID_rank = remove_redundancy_hybrid_ID(A, rank)
+            t1 = time.perf_counter() - t1
+            t2 = time.process_time() - t2
+            print("remove_redundancy_hybrid_ID took: ", t1, t2) 
+            performance_dict[filename].append(["remove_redundancy_hybrid_ID", t1, t2, hybrid_ID_rank])
 
-            t1 = time.time()
-            remove_redundancy_SVD(A)
-            t2 = time.time() - t1
-            print("remove_redundancy_SVD took: ", t2) 
-            performance_dict[filename].append(["remove_redundancy_SVD", t2])
+            t1 = time.perf_counter()
+            t2 = time.process_time()
+            SVD_rank = remove_redundancy_dense(A, rank)
+            t1 = time.perf_counter() - t1
+            t2 = time.process_time() - t2
+            print("remove_redundancy_dense took: ", t1, t2) 
+            performance_dict[filename].append(["remove_redundancy_dense", t1, t2, SVD_rank])
 
-            t1 = time.time()
-            remove_redundancy_sparse(sparse_A)
-            t2 = time.time() - t1
-            print("remove_redundancy_sparse took: ", t2) 
-            performance_dict[filename].append(["remove_redundancy_sparse", t2])
+            t1 = time.perf_counter()
+            t2 = time.process_time()
+            ID_rank = remove_redundancy_ID(A, rank)
+            t1 = time.perf_counter() - t1
+            t2 = time.process_time() - t2
+            print("remove_redundancy_ID took: ", t1, t2) 
+            performance_dict[filename].append(["remove_redundancy_ID", t1, t2, ID_rank])
+
+            # t1 = time.time()
+            # remove_redundancy_sparse(sparse_A)
+            # t2 = time.time() - t1
+            # print("remove_redundancy_sparse took: ", t2) 
+            # performance_dict[filename].append(["remove_redundancy_sparse", t2])
+
+            # data_saver(performance_dict)
 
         except Exception as e:
             print("An Exception Occured!", e)
 
     return performance_dict
+
 
 def save_file(data, path):
     try:
@@ -129,11 +194,18 @@ def save_file(data, path):
     except Exception as e:
         print("Failed to save!", e)
 
+def gen_data_saver(data_path):
+    def data_saver(data):
+        path = data_path
+        save_file(data, path)
+
+    return data_saver
 
 if __name__ == "__main__":
 
     # Name of the file containing a list of matrix names (by default located in the same directory.. ** Can be changed **)
-    fileName = "MatrixNames.txt" 
+    fileName = "All_MatrixNames.txt" 
+    # fileName = "All_MatrixNames.txt" 
     # path to the directory containin the matrices
     base_location = 'C:\Home\data\Singular Matrices' 
     # path of the file where the benchmarks will be saved
@@ -146,7 +218,8 @@ if __name__ == "__main__":
     elif len(sys.argv) == 3:
         fileName = sys.argv[1]
         save_location = sys.argv[2]
-        
-    benchmark = measure_performance()
+
+    data_saver = gen_data_saver(save_location)
+    benchmark = measure_performance(data_saver)
     # print(benchmark)
-    save_file(benchmark, save_location)
+    # save_file(benchmark, save_location)
