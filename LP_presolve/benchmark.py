5621827d3e908c3048d1bd54a147fb441e86d93b
Touqir <shuhash6@gmail.com>
2019-01-12 13:13:02 -0700

    fixed a bug

--- LP_presolve/benchmark.py
+++ LP_presolve/benchmark.py
@@ -1,6 +1,7 @@
 from scipy.io import loadmat
 import numpy as np
-from _matrix_compress import extract_independent_columns_hybrid_ID, extract_independent_columns_ID, _build_matrix_rank_k_FAST
+from _matrix_compress import extract_independent_columns_hybrid_ID, extract_independent_rows_hybrid_ID, extract_independent_columns_ID 
+from _matrix_compress import extract_independent_rows_ID, _build_matrix_rank_k_FAST
 import os.path 
 from scipy.optimize import _remove_redundancy
 import time
@@ -43,11 +44,11 @@ def load_matrix(fileName):
 
 
 def remove_redundancy_ID(A, rank):
-    cols = extract_independent_columns_ID(A, rank=rank)
-    A_new = A[:,cols]
+    # cols = extract_independent_columns_ID(A, rank=rank)
+    # A_new = A[:,cols]
 
-    # rows = extract_independent_rows_ID(A, rank=rank)
-    # A_new = A[:,rows]
+    rows = extract_independent_rows_ID(A, rank=rank)
+    A_new = A[:,rows]
 
     rank_new = matrix_rank(A_new)
     print("remove_redundancy_ID: ", rank_new == rank, rank_new, rank)
@@ -58,8 +59,8 @@ def remove_redundancy_ID(A, rank):
 
 
 def remove_redundancy_hybrid_ID(A, rank):
-    A_new, cols = extract_independent_columns_hybrid_ID(A, k=rank)
-    # A_new, rows = extract_independent_columns_hybrid_ID(A, k=rank)
+    # A_new, cols = extract_independent_columns_hybrid_ID(A, k=rank)
+    A_new, rows = extract_independent_rows_hybrid_ID(A, k=rank)
 
     rank_new = matrix_rank(A_new)
     print("remove_redundancy_hybrid_ID: ", rank_new == rank, rank_new, rank)
